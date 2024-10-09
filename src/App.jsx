86d0e987b1e28e5129204e07c082fe7b1e5a7930
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2024-10-09 13:26:09 +0530

    with help of get api display all added product and delete product id wise

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,50 @@
-  import React from "react";
-  import { BrowserRouter, Routes, Route } from "react-router-dom";
-  import Add from "./pages/Add";
-  import Navbar from "./components/Navbar";
-  import Footer from "./components/Footer";
-  import Sidebar from "./components/Sidebar";
-  import Orders from "./pages/Orders";
-  import List from "./pages/List";
-  import Login from "./components/Login";
-  import {Toaster} from 'react-hot-toast'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import Orders from "./pages/Orders";
+import List from "./pages/List";
+import Login from "./components/Login";
+import { Toaster } from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
 import { setToken } from "./Redux/auth/authSlice";
 
+export const currency = "₹";
 
-  const App = () => {
-    const token = useSelector((state)=>state.auth.token)
-    const dispatch = useDispatch();
+const App = () => {
+  const token = useSelector((state) => state.auth.token);
+  const dispatch = useDispatch();
 
-    return (
-      <BrowserRouter>
-        <div className="bg-gray-50 min-h-screen flex flex-col">
+  return (
+    <BrowserRouter>
+      <div className="bg-gray-50 min-h-screen flex flex-col">
         <Toaster position="top-center" />
-          {token === "" ? (
-            <Login setToken={(newToken)=>dispatch(setToken(newToken))} />
-          ) : (
-            <>
-              <Navbar setToken={setToken} />
-              <hr /> 
-              <div className="flex flex-1 w-full">
-                <Sidebar />
-                <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-                  <Routes>
-                    <Route path="/add" element={<Add token={token} />} />
-                    <Route path="/list" element={<List token={token} />} />
-                    <Route path="/order" element={<Orders token={token} />} />
-                    <Route path="/login" element={<Login />} />
-                  </Routes>
-                </div>
+        {token === "" ? (
+          <Login setToken={(newToken) => dispatch(setToken(newToken))} />
+        ) : (
+          <>
+            <Navbar setToken={setToken} />
+            <hr />
+            <div className="flex flex-1 w-full">
+              <Sidebar />
+              <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+                <Routes>
+                  <Route path="/add" element={<Add token={token} />} />
+                  <Route path="/list" element={<List token={token} />} />
+                  <Route path="/order" element={<Orders token={token} />} />
+                  <Route path="/login" element={<Login />} />
+                </Routes>
               </div>
-            </>
-          )}
+            </div>
+          </>
+        )}
 
-          <Footer />
-        </div>
-      </BrowserRouter>
-    );
-  };
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-  export default App;
+export default App;
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { currency } from "../App";
 
-const List = () => {
+const List = ({ token }) => {
+  const [list, setList] = useState([]);
+  const fetchInterval = 5000;
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get("/api/product/list");
+      if (response.data.success) {
+        setList(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const removeProduct = async (id) => {
+    try {
+      const response = await axios.delete(`/api/product/remove/${id}`, {
+        headers: { token },
+      });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+    // const intervalId = setInterval(fetchList, fetchInterval);
+    // return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div>List</div>
-  )
-}
+    <div>
+      <p className="mb-2">All Product List</p>
+      <div className="flex flex-col gap-2">
+        <div className="hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b className="text-center">Action</b>
+        </div>
+        {list.map((item) => (
+          <div
+            key={item._id}
+            className="grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm"
+          >
+            <img src={item.image[0]} alt="product-image" className="w-12" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>
+              {currency}
+              {item.price}
+            </p>
+            <p onClick={() => removeProduct(item._id)} className="text-right md:text-center cursor-pointer text-lg">
+              X
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
